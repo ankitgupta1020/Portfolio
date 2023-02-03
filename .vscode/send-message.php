dea4440f901cd7c82adb80ab12799c7b81fd3d73
ANKIT GUPTA <ankitgupta1181@gmail.com>
2023-02-03 23:41:22 +0530

    initial commit

--- .vscode/send-message.php
+++ .vscode/send-message.php
@@ -2,7 +2,7 @@
 
 $data = json_decode(file_get_contents("php://input"), true);
 
-$to = "your-email@example.com";
+$to = "ankitgupta1181@gmail.com";
 $subject = "New Message from Portfolio Website";
 $body = "Name: {$data['name']}\nEmail: {$data['email']}\nMessage: {$data['message']}";
 
