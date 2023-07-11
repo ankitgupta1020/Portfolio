f344df763da1b57e4797f2d35f1007f1f2e1c46e
Krishandeep Singh <krishandeep17@gmail.com>
2023-07-11 20:55:25 +0530

    add box shadow and resize header on scrolled down

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 const themeSwitch = document.getElementById("themeSwitch");
+const header = document.querySelector(".header");
 
 // Toggle between light and dark mode
 themeSwitch.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
+});
+
+// Add box shadow and resize header on scrolled down
+document.addEventListener("scroll", (e) => {
+  const scrollPosition = e.target.scrollingElement.scrollTop;
+
+  if (scrollPosition > 10) {
+    if (!header.classList.contains("scrolled-down")) {
+      header.classList.add("scrolled-down");
+    }
+  } else {
+    if (header.classList.contains("scrolled-down")) {
+      header.classList.remove("scrolled-down");
+    }
+  }
 });
