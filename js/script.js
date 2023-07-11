b70749fe7c969458f43905c4733b0278085a70a5
Krishandeep Singh <krishandeep17@gmail.com>
2023-07-11 22:04:06 +0530

    make project carousel work

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
-const themeSwitch = document.getElementById("themeSwitch");
+const themeSwitchBtn = document.getElementById("themeSwitch");
 const header = document.querySelector(".header");
+const carousel = document.querySelector(".carousel");
+const slides = Array.from(carousel.children);
+const prevBtn = document.getElementById("prevBtn");
+const nextBtn = document.getElementById("nextBtn");
+const dotsContainer = document.querySelector(".carousel-dots");
 
-// Toggle between light and dark mode
-themeSwitch.addEventListener("click", () => {
+// Function to toggle dark-mode class to the body
+function themeToggle() {
   document.body.classList.toggle("dark-mode");
-});
+}
 
-// Add box shadow and resize header on scrolled down
-document.addEventListener("scroll", (e) => {
+// Add event listeners to button
+themeSwitchBtn.addEventListener("click", themeToggle);
+
+// ------------------------------- //
+
+// Function to add scrolled-down class to the header while scrolling down
+function scrolledDown(e) {
   const scrollPosition = e.target.scrollingElement.scrollTop;
 
   if (scrollPosition > 10) {
@@ -19,4 +29,71 @@ document.addEventListener("scroll", (e) => {
       header.classList.remove("scrolled-down");
     }
   }
+}
+
+// Add event listeners for scroll
+document.addEventListener("scroll", scrolledDown);
+
+// ------------------------------- //
+
+// Track the current slide
+let currentSlide = 0;
+
+// Function to move the carousel to the next slide
+function moveToSlide(slideIndex) {
+  carousel.style.transform = `translateX(-${slideIndex * 100}%)`;
+  currentSlide = slideIndex;
+  updateDots();
+}
+
+// Function to handle the next button click
+function nextSlide() {
+  if (currentSlide === slides.length - 1) {
+    moveToSlide(0);
+  } else {
+    moveToSlide(currentSlide + 1);
+  }
+}
+
+// Function to handle the previous button click
+function prevSlide() {
+  if (currentSlide === 0) {
+    moveToSlide(slides.length - 1);
+  } else {
+    moveToSlide(currentSlide - 1);
+  }
+}
+
+// Function to update the active dot
+function updateDots() {
+  const dots = Array.from(dotsContainer.children);
+  dots.forEach((dot, index) => {
+    if (index === currentSlide) {
+      dot.classList.add("active");
+    } else {
+      dot.classList.remove("active");
+    }
+  });
+}
+
+// Create carousel navigation dots
+for (let i = 0; i < slides.length; i++) {
+  const dot = document.createElement("span");
+  dot.classList.add("dot");
+  dotsContainer.appendChild(dot);
+}
+
+// Add event listeners to buttons and dots
+nextBtn.addEventListener("click", nextSlide);
+prevBtn.addEventListener("click", prevSlide);
+
+dotsContainer.addEventListener("click", (event) => {
+  if (event.target.classList.contains("dot")) {
+    const clickedDot = event.target;
+    const dotIndex = Array.from(dotsContainer.children).indexOf(clickedDot);
+    moveToSlide(dotIndex);
+  }
 });
+
+// Set the initial active dot
+updateDots();
